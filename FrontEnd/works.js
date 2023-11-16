670b38f85134b2c4869045041291e8360872a11f
Caldwell63 <oliveirateduina@gmail.com>
2023-11-16 04:22:28 +0100

    Filters and specs done

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,14 +1,21 @@
+import { addFilterListener } from "./filters.js";
+
 async function getWorks(){
     const response = await fetch("http://localhost:5678/api/works");
     return response.json()
 }
 
-function genererProjets(projets){
-    // const baliseTitre = document.createElement('h2')
-    // baliseTitre.textContent = "Mes Projets"
-    for (let i = 0; i < projets.length; i++) {
+async function getCategories(){
+    const category = await fetch("http://localhost:5678/api/categories");
+    return category.json()
+}
 
-        const works = projets[i];
+
+function generateProjects(projects){
+
+    for (let i = 0; i < projects.length; i++) {
+        //TODO : faire une boucle maps !!
+        const works = projects[i];
         // Récupération de l'élément du DOM qui accueillera les projets
         const divGallery = document.querySelector('.gallery');
         // Création d’une balise dédiée à un projet
@@ -23,11 +30,50 @@ function genererProjets(projets){
         divGallery.appendChild(projetElement);
         projetElement.appendChild(imageElement);
         projetElement.appendChild(nomElement);
-    
     }
+    
+}
 
+function generateFilterCategories(categories, works){
+    // Récupération de l'élément du DOM qui accueillera les catégories
+    const portfolio = document.getElementById('portfolio')
+    // Création de la balise menu dédiée aux filtres
+    const menu = document.getElementById('menu')
+    
+    portfolio.appendChild(menu);
+
+    const place = document.getElementById('gallery')
+
+    place.before(menu)
+    
+    categories.map((category) => {
+        const bouton = document.createElement('button')
+        bouton.classList.add('filter')
+        // bouton.classList.add('style')
+        bouton.innerHTML = category.name
+        // On rattache la balise 
+        menu.appendChild(bouton)  
+    })
 }
 
 const works = await getWorks()
-console.log(works)
-genererProjets(works)
+
+generateProjects(works)
+const categories = await getCategories()
+
+generateFilterCategories(categories,works)
+
+Array.from(document.getElementsByClassName('filter')).map((bouton) => {
+    bouton.addEventListener('click', function(event) {
+        const buttonCategory = event.target.innerText
+        console.log(buttonCategory)
+        document.querySelector('#gallery').innerHTML = '' 
+        if (buttonCategory != 'Tous') {
+            const filteredWorks = works.filter((work) => work.category.name == buttonCategory)
+            console.log(filteredWorks)
+            generateProjects(filteredWorks)
+        } else {
+            generateProjects(works)
+        }
+    })
+})
